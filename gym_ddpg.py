86b9d44bdf9d423a7be98a211bbd394c2750eaee
drc <yranidro@gmail.com>
2017-06-19 00:02:23 -0700

    update for Object Transition

--- gym_ddpg.py
+++ gym_ddpg.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 from os.path import isdir
 from os import mkdir
 
-ENV_NAME = 'MountainCarContinuous-v0'
-PATH = 'experiments/' + ENV_NAME + '-E5/'
+#ENV_NAME = 'MountainCarContinuous-v0'
+ENV_NAME = 'ObjectTransition-v0'
+PATH = 'experiments/' + ENV_NAME + '-E1/'
 EPISODES = 100000
 TEST = 3
 
@@ -30,7 +31,7 @@ def main():
         print "episode:",episode
         # Train
         for step in xrange(env.spec.timestep_limit):
-            #env.render()
+            env.render()
             action = agent.noise_action(state)
             next_state,reward,done,_ = env.step(action)
             #print('state={}, action={}, reward={}, next_state={}, done={}'.format(state, action, reward, next_state, done))
@@ -48,38 +49,39 @@ def main():
         # Testing:
         #if episode % 1 == 0:
         if episode % 25 == 0:
-			if not isdir(PATH):			
-				mkdir(PATH)
-			agent.save_model(PATH, episode)	
-			total_return = 0
-			total_reward = 0
-			for i in xrange(TEST):
-				env.render()				
-				state = env.reset()
-				reward_per_step = 0
-				for j in xrange(env.spec.timestep_limit):
-					#env.render()
-					action = agent.action(state) # direct action for test
-					state,reward,done,_ = env.step(action)
-					total_return += reward
-					if done:
-						break
-					reward_per_step += (reward - reward_per_step)/(j+1)
-				total_reward += reward_per_step
-			
-			ave_return = total_return/TEST
-			ave_reward = total_reward/TEST
-			returns.append(ave_return)
-			rewards.append(ave_reward)
-			
-			ax_avg_res[0].clear()
-			ax_avg_res[1].clear()
-			ax_avg_res[0].plot(returns)
-			ax_avg_res[1].plot(rewards)
-			fig_avg_res.show()
-			plt.pause(0.005)
+		#env.render()			
+		if not isdir(PATH):			
+			mkdir(PATH)
+		agent.save_model(PATH, episode)	
+		total_return = 0
+		total_reward = 0
+		for i in xrange(TEST):
+			env.render()				
+			state = env.reset()
+			reward_per_step = 0
+			for j in xrange(env.spec.timestep_limit):
+				#env.render()
+				action = agent.action(state) # direct action for test
+				state,reward,done,_ = env.step(action)
+				total_return += reward
+				if done:
+					break
+				reward_per_step += (reward - reward_per_step)/(j+1)
+			total_reward += reward_per_step
+	
+		ave_return = total_return/TEST
+		ave_reward = total_reward/TEST
+		returns.append(ave_return)
+		rewards.append(ave_reward)
+	
+		ax_avg_res[0].clear()
+		ax_avg_res[1].clear()
+		ax_avg_res[0].plot(returns)
+		ax_avg_res[1].plot(rewards)
+		fig_avg_res.show()
+		plt.pause(0.005)
 
-			print 'episode: ',episode,'Evaluation Average Return:',ave_return, '  Evaluation Average Reward: ', ave_reward
+		print 'episode: ',episode,'Evaluation Average Return:',ave_return, '  Evaluation Average Reward: ', ave_reward
     env.monitor.close()
 
 if __name__ == '__main__':
